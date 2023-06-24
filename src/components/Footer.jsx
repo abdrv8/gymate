3d5539d8a02e3a2bc2229379101a897015a5e3eb
Razaq <razaqonweb@gmail.com>
2023-06-24 10:55:55 +0100

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,70 @@
 import "./styles/Footer.scss";
 import { Container } from "./index";
+import { logoBlack } from "../assets";
 
 const Footer = () => {
   return (
     <div className="footer">
       <Container>
         <div className="content">
-          <h1>Footer</h1>
+          <div className="left">
+            <img src={logoBlack} alt="logo black" />
+            <div className="desc">
+              Take your health and body to the next level with our comprehensive
+              program designed to help you reach your fitness goals.
+            </div>
+            <div className="icons">
+              <i className="fa-brands fa-facebook-f"></i>
+              <i className="fa-brands fa-twitter"></i>
+              <i className="fa-brands fa-pinterest-p"></i>
+              <i className="fa-brands fa-youtube"></i>
+            </div>
+            <div className="copyright">
+              <p>Privacy Policy | &copy; {new Date().getFullYear()}, Gymate</p>
+              <p className="dev">
+                Built by:{" "}
+                <a
+                  href="https://razaq.netlify.app"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Razaq
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="center">
+            <div className="title">
+              <h2>Our Classes</h2>
+              <div className="line"></div>
+            </div>
+            <ul>
+              <li>Fitness Classes</li>
+              <li>Aerobics Classes</li>
+              <li>Power Yoga</li>
+              <li>Learn Machines</li>
+              <li>Full-body Strength</li>
+            </ul>
+          </div>
+          <div className="right">
+            <div className="title">
+              <h2>Working Hours</h2>
+              <div className="line"></div>
+            </div>
+            <ul>
+              <li>
+                <b>Monday - Friday:</b>
+              </li>
+              <li>7:00am - 21:00pm</li>
+              <li>
+                <b>Saturday:</b>
+              </li>
+              <li>7:00am - 19:00pm</li>
+              <li>
+                <b>Sunday - Closed</b>
+              </li>
+            </ul>
+          </div>
         </div>
       </Container>
     </div>
